8d93d1aef592eea25c3ab3976174972c08b59d81
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-05-23 21:33:08 +0200

    swiperJS set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'swiperCarousel';
+
+  slides = [
+    { name: 'cola', price: 20, color: 'red' },
+    { name: 'fanta', price: 20, color: 'blue' },
+    { name: 'chips', price: 20, color: 'green' },
+    { name: 'ice cream', price: 20, color: 'orange' },
+  ];
 }
